142b684e3c4bc9d334df0f85859758b1d8fc192c
AlexLonberg <alexlonberg@gmail.com>
2024-06-01 20:12:38 +0300

    fix

--- prebuild.js
+++ prebuild.js
@@ -1,24 +1,28 @@
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { mkdirSync, copyFileSync } from 'node:fs'
-import { clearDir } from './dist/dirs.js'
+import { mkdirSync } from 'node:fs'
+import { clearDir, copyDir } from './dist/dirs.js'
 import { makeModifier } from './dist/marker.js'
 import { rwModify } from './dist/publicrw.js'
 
-// Перед вызовом npm run build:npm должен быть вызван build
+// NOTE: Перед вызовом npm run build:npm должен быть вызван build для использования `from './dist/*'`
+// После необходимо внести правки на ссылки
+// ![](stage.png) - https://raw.githubusercontent.com/AlexLonberg/nodejs-pkg-tools/master/stage.png
+// [src/errors.ts](src/errors.ts) - https://github.com/AlexLonberg/nodejs-pkg-tools/blob/master/src/errors.ts
+// [файл 📄](src/types.ts) - https://github.com/AlexLonberg/nodejs-pkg-tools/blob/master/src/types.ts
 
 const wd = dirname(fileURLToPath(import.meta.url))
 const dest = join(wd, 'npm')
 
 if (clearDir(dest) === false) throw new Error('Ошибка очистки каталога npm.')
 mkdirSync(dest, { recursive: true })
-copyFileSync(join(wd, 'LICENSE.md'), join(dest, 'LICENSE.md'))
 
 const modifier = makeModifier((value) => [true, true,
   typeof value === 'string'
     ? value.replace('dist/', '')
     : (() => { throw new Error('Невалидное значение.') })()
 ])
+
 if (rwModify({
   mode: 'strict_error',
   include: [
@@ -40,3 +44,9 @@ if (rwModify({
   }
 }, join(wd, 'package.json'), join(dest, 'package.json'))
   .errors.isFatalError) throw new Error('Ошибка записи package.json')
+
+if (copyDir(wd, dest, ['LICENSE.md', 'README.md']) !== true) {
+  throw new Error('Ошибка копирования файлов.')
+}
+
+console.log('\x1b[41m \x1b[37m ВНИМАНИЕ: В файл README.md надо внести правки адресов stage.png, src/errors.ts, src/types.ts \x1b[0m')
